f5a334cb149b144e5406c77d0ad6f15d4dc63991
LUAN CUNHA <luan.cunha@viavarejo.com.br>
2024-01-27 21:36:07 -0300

    chore: modificado St

--- StressTest/IBurguerST.js
+++ StressTest/IBurguerST.js
@@ -4,14 +4,12 @@ import { Counter } from 'k6/metrics';
 
 export let options = {
   stages: [
-    { duration: '2m', target: 200 }, // Ramp down to 200 VUs over 2 minutes
-    { duration: '1m', target: 300 },
-    { duration: '1m', target: 350 }, 
-    { duration: '1m', target: 0 },  // Ramp down to 0 VUs over 1 minute
+    { duration: '6m', target: 1000 }
   ],
 };
 
-let myCounter = new Counter('my_counter');
+let requestCounter = Counter('total_requests');
+let errorCounter = Counter('total_errors');
 
 export default function () {
   
@@ -19,26 +17,29 @@ export default function () {
   const payload = { customerId: '041b23ad-3b11-441e-bab8-bb9270a09ae1' };
   const params = { headers: { 'Content-Type': 'application/json' } };
   const response1 = http.post('http://localhost:5000/api/carts', JSON.stringify(payload), params);
-  console.log(`Create shoppingCart - Status Code: ${response1.status}`);
-  myCounter.add(1);
-
+  logResponse('Create shoppingCart', response1);
   sleep(1);
 
   // Get Orders
   const response2 = http.get('http://localhost:5000/api/orders');
-  console.log(`Get Orders - Status Code: ${response2.status}`);
-  myCounter.add(1);
+  logResponse('Get Orders', response2);
   sleep(1);
 
    // Get queued Orders
    const response3 = http.get('http://localhost:5000/api/orders/queue');
-   console.log(`Get queued Orders - Status Code: ${response3.status}`);
-   myCounter.add(1);
+   logResponse('Get queued Orders', response3);
    sleep(1);
  
 }
 
-// Print the total number of requests after the test completes
-export function teardown(data) {
-  console.log(`Total Requests: ${myCounter.value}`);
+// Function to log response details and errors
+function logResponse(requestName, response) {
+  console.log(`${requestName} - Status Code: ${response.status}`);
+  requestCounter.add(1);
+
+  // Check for errors and log error details
+  if (response.status !== 200) {
+    console.error(`${requestName} - Error - Status Code: ${response.status}`);
+    errorCounter.add(1);
+  }
 }
